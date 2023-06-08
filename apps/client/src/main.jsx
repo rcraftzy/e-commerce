16dc907ec8b524c3d0b7a1c9a83bd3884e638058
Speedev <roberto.tg99@gmail.com>
2023-06-08 10:35:35 -0500

    :feat: Add imports for carousel swiper

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/styles/index.css'
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
