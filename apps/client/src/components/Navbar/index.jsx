e31114262e1bc035b1b62b177736a23b936dd26d
Speedev <roberto.tg99@gmail.com>
2023-06-15 15:14:18 -0500

    :sparkles: Add a item 'pide aqui/tu carrito de compra' for responsive mode

--- apps/client/src/components/Navbar/index.jsx
+++ apps/client/src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import { DropdownMenu } from "./DropdownMenu"
 import { HamburguesaIcon } from "../Icons"
+import { MenuShoppingCard } from "../MenuShoppingCard"
 
 export const links = [
   {
@@ -26,6 +27,8 @@ export const links = [
 ]
 
 export const Navbar = () => {
+  const location = useLocation()
+  const { pathname } = location
   const baseLinkClass = "text-lg border-b-4 hover:text-intense-orange hover:border-intense-orange text-chocolate-brown font-bold transition ease"
   const linkClass = `${baseLinkClass} border-intense-orange text-intense-orange`
 
@@ -51,21 +54,48 @@ export const Navbar = () => {
                 </NavLink>
               </li>
             ))}
-            <li className="ml-2 h-full bg-fire-red flex items-center">
-              <a href="" className="flex flex-col justify-center px-9 gap-1">
-                <div className="w-20 h-auto fill-white">
-                  <HamburguesaIcon />
-                </div>
-                <span className="text-center font-bold text-white">Pide aquí</span>
-              </a>
-            </li>
+            {
+              pathname === "/menu" 
+                ?
+                (
+                  <li className="ml-2 h-full bg-fire-red flex items-center">
+                    <MenuShoppingCard />
+                  </li>
+                )
+                : (<li className="ml-2 h-full bg-fire-red flex items-center">
+                  <a href="" className="flex flex-col justify-center px-9 gap-1">
+                    <div className="w-20 h-auto fill-white">
+                      <HamburguesaIcon />
+                    </div>
+                    <span className="text-center font-bold text-white">Pide aquí</span>
+                  </a>
+                </li>)
+            }
           </ul>
         </nav>
         <DropdownMenu />
-        <div className="hidden">
-          Pide aquí
+        <div className="absolute right-0 top-40 md:hidden">
+          {
+            pathname === "/menu" 
+              ?
+              (
+                <li className="h-full bg-fire-red flex items-center">
+                  <MenuShoppingCard />
+                </li>
+              )
+              : (<li className="h-full bg-fire-red flex items-center">
+                <a href="" className="flex flex-col justify-center py-5 pl-8 pr-12 gap-1">
+                  <div className="w-20 h-auto fill-white">
+                    <HamburguesaIcon />
+                  </div>
+                  <span className="text-center font-bold text-white">Pide aquí</span>
+                </a>
+              </li>)
+          }
         </div>
       </div>
     </header>
   )
 }
+
+
