1b3cb505c7d4c9a8673584cb2851d100f841937f
Speedev <roberto.tg99@gmail.com>
2023-06-15 23:33:11 -0500

    :sparkles: Add responsive layout main

--- apps/client/src/hocs/layouts/FullClientLayout.jsx
+++ apps/client/src/hocs/layouts/FullClientLayout.jsx
@@ -5,7 +5,7 @@ export const FullClientLayout = () => {
   return (
     <section className='font-tahoma bg-light-ivory relative m-0 p-0 min-h-full min-w-full'>
       <Navbar />
-      <main className='pt-32'>
+      <main className='pt-24 sm:pt-28'>
         <Outlet />
       </main>
       <Footer />
