427b0384d18a34c0e9618da452dfc09e75c431aa
Speedev <roberto.tg99@gmail.com>
2023-06-16 19:17:04 -0500

    :sparkles: Update layout for move styles in body to index.cs

--- apps/client/src/hocs/layouts/FullClientLayout.jsx
+++ apps/client/src/hocs/layouts/FullClientLayout.jsx
@@ -3,12 +3,12 @@ import { Navbar, Footer } from '../../components'
 
 export const FullClientLayout = () => {
   return (
-    <section className='font-tahoma bg-light-ivory relative m-0 p-0 min-h-full min-w-full'>
+    <>
       <Navbar />
       <main className='pt-24 sm:pt-28'>
         <Outlet />
       </main>
       <Footer />
-    </section>
+    </>
   )
 }
