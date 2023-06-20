6e55c1bdb2110f0ebb89104ec1e3c67419df8460
Speedev <roberto.tg99@gmail.com>
2023-06-20 12:11:43 -0500

    :sparkles: Add background image for item

--- apps/client/src/containers/pages/Home/index.jsx
+++ apps/client/src/containers/pages/Home/index.jsx
@@ -12,14 +12,15 @@ export const Home = () => {
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
-        }}
-        navigation={true}
-        modules={[Autoplay, Navigation]}
-      >
+          }}
+          className="mt-6"
+          navigation={true}
+          modules={[Autoplay, Navigation]}
+        >
           <SwiperSlide>
-            <BackgroundImage className={"bg-auto"} image={"/assets/banners/Banner-4.png"}>
-              <div className="mt-60 lg:mt-72 sm:pb-4 sm:pt-9 md:pt-5 mx-auto max-w-5xl px-11 sm:px-14">
-                <section className="flex flex-col">
+            <div className="mt-14">
+              <BackgroundImage className={"bg-auto"} image={"/assets/banners/Banner-4.png"}>
+                <section className="pt-80 px-20 pb-16 flex flex-col">
                   <h3 className="flex justify-center z-10 sm:justify-start text-center font-ifc-insane-rodeo-bold text-3xl sm:text-[42px] md:text-5xl mb-2 text-light-gold drop-shadow-[0_6px_4px_rgba(0,0,0,0.4)]">Nos encargamos de tu</h3>
                   <div className="inline-flex items-center justify-center sm:justify-start h-11 mb-6">
                     <span className="font-ifc-insane-rodeo-bold text-8xl z-10 lg:text-9xl text-light-ivory drop-shadow-[0_6px_4px_rgba(0,0,0,0.4)] mb-4">EVENTO</span>
@@ -28,8 +29,8 @@ export const Home = () => {
                     <a className="text-chocolate-brown bg-vibrant-yellow px-11 py-4 text-sm font-bold">Consulta aqui</a>
                   </span>
                 </section>
-              </div>
             </BackgroundImage>
+            </div>
           </SwiperSlide>
      </Swiper>
       </section>
