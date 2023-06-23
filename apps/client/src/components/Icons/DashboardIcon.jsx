5ec5bfd94a620e8144286282f5ef623ad09f8688
Speedev <roberto.tg99@gmail.com>
2023-06-23 14:32:45 -0500

    :sparkles: Feat icons for sidebar

---
+++ apps/client/src/components/Icons/DashboardIcon.jsx
@@ -0,0 +1,1 @@
+export const DashboardIcon = () => <svg version="1.1" x="0" y="0" viewBox="0 0 36 36"><path d="M19.5 13.5V0H36v13.5H19.5zM0 19.5V0h16.5v19.5H0zM19.5 36V16.5H36V36H19.5zM0 36V22.5h16.5V36H0zm3-19.5h10.5V3H3v13.5zM22.5 33H33V19.5H22.5V33zm0-22.5H33V3H22.5v7.5zM3 33h10.5v-7.5H3V33z"/></svg>
--- apps/client/src/components/Sidebar/index.jsx
+++ apps/client/src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
-import { BurgerIcon } from "../Icons"
+import { BurgerIcon, DashboardIcon, ProductIcon, TicketIcon } from "../Icons"
+import { BannerIcon } from "../Icons/BannerIcon"
 
 export const Sidebar = () => {
   return (
@@ -7,36 +8,36 @@ export const Sidebar = () => {
         <ul className="pt-4 flex flex-col gap-8">
           <li className="flex justify-center">
             <a href="#">
-              <div className="w-10 fill-white">
+              <div className="w-9 fill-white">
                 <BurgerIcon />
               </div>
             </a>
           </li>
           <li className="flex justify-center">
             <a href="#dashboard" className="pointer">
-              <div className="w-10 fill-white">
-                <BurgerIcon />
+              <div className="w-9 fill-white">
+                <DashboardIcon />
               </div>
             </a>
           </li>
           <li className="flex justify-center">
             <a href="#banners">
-              <div className="w-10 fill-white">
-                <BurgerIcon />
+              <div className="w-9 fill-white">
+                <BannerIcon />
               </div>
             </a>
           </li>
           <li className="flex justify-center">
             <a href="#productos">
-              <div className="w-10 fill-white">
-                <BurgerIcon />
+              <div className="w-9 fill-white">
+                <ProductIcon />
               </div>
             </a>
           </li>
           <li className="flex justify-center">
             <a href="#tickets">
-              <div className="w-10 fill-white">
-                <BurgerIcon />
+              <div className="w-9 fill-white">
+                <TicketIcon />
               </div>
             </a>
           </li>
