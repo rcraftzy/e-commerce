9a0620ecb8c17dcde78a0885effa083c92aa8944
Speedev <roberto.tg99@gmail.com>
2023-06-23 19:13:28 -0500

    :bug: Fix button to close

--- apps/client/src/components/Navigator/DropdownBubble.jsx
+++ apps/client/src/components/Navigator/DropdownBubble.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { ChevronDown, ExitIcon } from "../Icons"
+import { ChevronDown } from "../Icons"
 import { useEffect, useRef, useState } from "react"
 import styles from './style.module.css'
 
@@ -13,37 +13,35 @@ export const DropdownExit = () => {
         setIsOpen(false);
       }
     };
-
     document.addEventListener('mousedown', handleClickOutside);
-
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
   const toggleMenu = () => setIsOpen(!isOpen)
+
   return (
-    <div className="">
       <div className="dropdown items-center">
-        <button
-          className='w-9 h-9 flex justify-center items-center'
-          onClick={toggleMenu}
-        >
-          {isOpen ?
-            <div className="fill-chocolate-brown w-4">
-              <ExitIcon />
-            </div>
-            :
-            <div className="stroke-chocolate-brown stroke-2 w-8">
-              <ChevronDown />
-            </div>
-          }
-          
-        </button>
-        {isOpen && (
+      <div
+        className='w-9 h-9 flex justify-center items-center'
+        onClick={toggleMenu}
+      >
+        {isOpen ?
+          <div className="absolute stroke-chocolate-brown stroke-2 w-8">
+            <ChevronDown />
+          </div>
+          :
+          <div className="stroke-chocolate-brown stroke-2 w-8">
+            <ChevronDown />
+          </div>
+        }
+      </div>
+      {isOpen && (
+        <div className="absolute inset-0 bg-inherit hover:cursor-pointer h-24">
           <div
             ref={refComponente}
-            className={`origin-top-right absolute right-0 mt-6 w-56`}
+            className={`origin-top-right absolute right-0 bottom-0 w-56`}
           >
             <div className={`relative rounded-l-lg rounded-br-lg bg-white shadow-xs`}>
               <div className={`${styles.triangle} absolute -top-6 bg-white right-0 text-white`}>Hola</div>
@@ -61,8 +59,8 @@ export const DropdownExit = () => {
               </div>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
